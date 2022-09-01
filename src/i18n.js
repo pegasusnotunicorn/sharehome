a66dc7b72abd636ce920c464b2a454cc15fb9111
Wonmin Lee <won2blee@gmail.com>
2022-08-31 22:50:29 -0400

    language set to eng

--- src/i18n.js
+++ src/i18n.js
@@ -18,6 +18,8 @@ i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
+    lng: "en-US",
+    supportedLngs: ["en-US", "ja"],
     fallbackLng: "en-US",
     // lng: "en-US", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage

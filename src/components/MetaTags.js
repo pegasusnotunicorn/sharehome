076a48eace36e867408fc6fb12f5188b5ff7693a
Wonmin Lee <won2blee@gmail.com>
2022-09-29 18:12:32 -0400

    new meta tag image

--- src/components/MetaTags.js
+++ src/components/MetaTags.js
@@ -3,7 +3,7 @@ import React from 'react';
 import CustomHelmet from './utils/CustomHelmet.js';
 
 const MetaTags = (props) => {
-  let splashImage = "/images/mainbox.jpg";
+  let splashImage = "/images/main.jpg";
   let description = "A fantastical reality TV card game about laughing, acting, and storytelling. Form relationships, stir up drama, and achieve your secret life goals!"
   let title = "Love, Career & Magic — A SHAREHOME Game";
 

52fec7ba9057cca9ddba920d5703fdb5bba48483
Wonmin Lee <pegasusnotunicorn@gmail.com>
2025-01-31 22:22:21 -0500

    fix to og image and description

--- src/components/MetaTags.js
+++ src/components/MetaTags.js
@@ -2,7 +2,7 @@ import CustomHelmet from "./utils/CustomHelmet.js";
 
 const MetaTags = () => (
   <CustomHelmet
-    title="Love, Career & Magic - 12 minute game for 2-6 players"
+    title="Love, Career & Magic - 12m game for 2-6 players"
     splashImage="/images/main.webp"
     description="A party game where fantasy meets reality TV. Play as a mythical creature, complete chaotic character arcs, and achieve high ratings for the season!"
   />

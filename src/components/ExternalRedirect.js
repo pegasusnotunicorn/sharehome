afd9c42733369a156bee37f03d048b0c8e6901c2
Wonmin Lee <pegasusnotunicorn@gmail.com>
2025-09-04 17:55:39 -0400

    keep utm params for /buy route

--- src/components/ExternalRedirect.js
+++ src/components/ExternalRedirect.js
@@ -2,7 +2,31 @@ import { useEffect } from "react";
 
 const ExternalRedirect = ({ url }) => {
   useEffect(() => {
-    window.location.replace(url);
+    // Get current URL parameters
+    const currentUrl = new URL(window.location.href);
+    const currentParams = currentUrl.searchParams;
+
+    // Create the target URL with UTM parameters
+    const targetUrl = new URL(url);
+
+    // Preserve UTM parameters and other tracking parameters
+    const trackingParams = [
+      "utm_source",
+      "utm_medium",
+      "utm_campaign",
+      "utm_term",
+      "utm_content",
+      "gclid",
+      "fbclid",
+    ];
+
+    trackingParams.forEach((param) => {
+      if (currentParams.has(param)) {
+        targetUrl.searchParams.set(param, currentParams.get(param));
+      }
+    });
+
+    window.location.replace(targetUrl.toString());
   }, [url]);
 
   return null; // No UI needed
